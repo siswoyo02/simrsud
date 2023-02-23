2dc5c11ab116b6b3dc5652812b3f7aab1c275225
siswoyo <siswoyo@gmail.com>
2023-02-23 08:28:53 +0700

    kedua

---
+++ app/Models/Rpasienbalitaplg.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rpasienbalitaplg extends Model
+{
+    use HasFactory;
+}
